ddc62a21d7ca3eed4028235788d06a0c442f2f02
angelinaf071006 <angelinaf0006@gmail.com>
2025-09-21 12:56:52 -0400

    Update visualize_attention.py

--- Pre-training-and-Training/visualize_attention.py
+++ Pre-training-and-Training/visualize_attention.py
@@ -7,7 +7,7 @@ def plot_self_attention(model_dir, sequence, layer=-1, average_heads=True, save_
     """
     Generate and plot a self-attention heatmap for a given DNA sequence.
     """
-   tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
     model = AutoModel.from_pretrained(model_dir, output_attentions=True, trust_remote_code=True)
     model.eval()
 
